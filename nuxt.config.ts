6ae04595520f8b655736815128016d83610aed4d
Salvatore Vaccarino <salvo@lunarct.com>
2025-03-04 01:16:31 +0000

    fixing action

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,5 +14,10 @@ export default defineNuxtConfig({
   i18n: {
     vueI18n: './i18n.config.ts' // if you are using custom path, default
   },
-  ssr: false
+  ssr: false,
+  routeRules: {
+    '/': {
+      prerender: true
+    }
+  }
 })
